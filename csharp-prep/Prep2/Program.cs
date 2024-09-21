c69653a6f43da139d920007abb3d2a99a63344e4
Austin Jonas <austinjonas@Austins-MacBook-Air-2.local>
2024-09-20 18:27:36 -0600

    made programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("");
+        Console.WriteLine("Hello! I want to tell you your letter grade. Type in your % here: ");
+        string gradeNumber = Console.ReadLine();
+        int number = int.Parse(gradeNumber);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number <= 59)
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade letter is: {letter}.");
+        
+        if (number <= 70)
+        {
+            Console.WriteLine("You have failed the class and will need to take the course again.");
+            Console.WriteLine("");
+        }
+        if (!(number <=70))
+        {
+            Console.WriteLine("You passed the class!");
+            Console.WriteLine("");
+        }
     }
 }
