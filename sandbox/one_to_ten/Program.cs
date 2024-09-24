8fb48ccc91b033e7a08070d910e2dbf4a0eba4c0
Austin Jonas <mail@austinjonas.com>
2024-09-24 11:43:04 -0600

    modifications to programs

---
+++ sandbox/one_to_ten/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for ( int i=1; i <= 10; i += 1) // the last one (modifier) is the action that will be happening in the loop, so adding 1 each time in this case.
+{
+    Console.WriteLine(i);
+}
