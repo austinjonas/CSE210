3c4c2d7886a79e10b58f8f78e6950be91f523d2b
Austin Jonas <mail@austinjonas.com>
2024-09-24 11:42:31 -0600

    made magic number guess program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 6);
+
+        int guess = 0;
+        
+        while ( guess!= magicNumber)
+        {
+            Console.Write("What is your guess ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("That's correct!");
+            }
+            else if (guess <= magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess >= magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
