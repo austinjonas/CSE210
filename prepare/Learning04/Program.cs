0f729ae75bcb195518bac4a3825a80ad5825629b
Austin Jonas <mail@austinjonas.com>
2024-09-26 16:52:54 -0600

    built the final learn program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // creates a new list called "numbers"
+        List<int> numbers = new List<int>();
+
+        int inputNumber = -1;
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");   
+            //user input option that converts their input to an int.
+            string input = Console.ReadLine();
+            inputNumber = int.Parse(Console.ReadLine());
+            
+            //adds numbers to the list
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+            
+        }
+        
+        //compute and print the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}. ");
+        
+        //compute the average
+        //this sets the word average as a float variable, after the =, it runs the
+        //previously made sum as a float, then counts the total items in the numbers list
+        //by using the .Count operation.
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}. ");
+
+        //compute and print the max
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The max is {maxNumber} ");
+
+
+        
     }
 }
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
+    
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        DisplayMessage();
+        
+        string username = GetUserName("Please enter your name: ");
+        int userNumber = FavoriteNumber();
+        
+        int squareNumber = SquareNumber(userNumber);
+        
+        DisplayResult(username, squareNumber);
+         
+    }    
+
+    //this is the welcome message
+    static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+    
+    static string GetUserName(string prompt)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            
+            return name;
+        }
+
+    static int FavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favNumber = Console.ReadLine();
+        int number = int.Parse(favNumber);
+        
+        return number;
+
     }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, your square number is {square}.");
+    }
+        
+    
 }
